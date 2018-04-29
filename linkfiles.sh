60084407215b02f0204377bf9e8027833c4ae3bc
Gianpiero Addis <g.addis@lemundo.de>
2018-04-29 15:05:37 +0200

    Install Vundle and Monokai with the setup script

--- linkfiles.sh
+++ linkfiles.sh
@@ -15,3 +15,16 @@ function makeSymLink {
 makeSymLink $PWD/.vimrc $HOME/.vimrc
 makeSymLink $PWD/plugins.vim $HOME/.vim/plugins.vim
 makeSymLink $PWD/.aliases $HOME/.aliases
+
+# Install Vundle if it is not present
+if [ ! -d "$HOME/.vim/bundle/Vundle.vim"  ]; then
+    echo "Vundle is not installed. Installation..."
+    git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+fi
+
+#Install monokai theme
+if [ ! -f "$HOME/.vim/colors/monokai.vim" ]; then
+    echo "The monokai color scheme is not installed. Installation..."
+    wget https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim -O $HOME/.vim/colors/monokai.vim
+fi
+
