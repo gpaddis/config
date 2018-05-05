a056fe8c1c84e0dccb2af32d73170328119bd371
Gianpiero Addis <gianpiero.addis@gmail.com>
2018-05-05 11:41:18 +0200

    Extract all functions to a dedicate file

--- setup.sh
+++ setup.sh
@@ -6,38 +6,7 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 VIMDIR="$HOME/.vim"
 
-# Make a symlink to origin in destination if this does not exist
-function makeSymLink {
-    local origin="$1" destination="$2"
-    if [ ! -f $destination ]
-    then
-        ln -s $origin $destination
-        printf "${GREEN}Linked $origin to $destination.${NC}\n"
-    else printf "${GREEN}The file $destination already exists.${NC}\n"
-    fi
-}
-
-# Check if a directory exists or create the full path
-function checkOrMakeDir {
-    local directory="$1"
-    if [ ! -d $directory ]; then
-        mkdir -p $directory
-    fi
-}
-
-# If the destination file does not exist download from origin to destination
-function checkOrDownload {
-    local origin="$1" destination="$2" downloadCommand="wget"
-
-    if [ ! -f $destination ]; then
-        printf "${GREEN}Could not find $destination. Installation...${NC}\n"
-        # Use wget or curl
-        if [ $(command -v wget) ]
-        then wget $origin -O $destination
-        else curl -o $destination $origin
-        fi
-    fi
-}
+source bash_functions.sh
 
 makeSymLink $PWD/.vimrc $HOME/.vimrc
 makeSymLink $PWD/plugins.vim $VIMDIR/plugins.vim
