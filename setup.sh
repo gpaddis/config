452d1f0deee54b3f56c6d3d299e9bb87687a95b2
Gianpiero Addis <gianpiero.addis@gmail.com>
2018-05-05 10:42:13 +0200

    Add curl-compatible checkordownload

--- setup.sh
+++ setup.sh
@@ -4,12 +4,13 @@
 
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
+VIMDIR="$HOME/.vim"
 
 # Make a symlink to origin in destination if this does not exist
 function makeSymLink {
     local origin="$1" destination="$2"
     if [ ! -f $destination ]
-    then 
+    then
         ln -s $origin $destination
         printf "${GREEN}Linked $origin to $destination.${NC}\n"
     else printf "${GREEN}The file $destination already exists.${NC}\n"
@@ -24,21 +25,33 @@ function checkOrMakeDir {
     fi
 }
 
+# If the destination file does not exist download from origin to destination
+function checkOrDownload {
+    local origin="$1" destination="$2" downloadCommand="wget"
+
+    if [ ! -f $destination ]; then
+        printf "${GREEN}Could not find $destination. Installation...${NC}\n"
+        # Use wget or curl
+        if [ $(command -v wget) ]
+        then wget $origin -O $destination
+        else curl -o $destination $origin
+        fi
+    fi
+}
+
 makeSymLink $PWD/.vimrc $HOME/.vimrc
-makeSymLink $PWD/plugins.vim $HOME/.vim/plugins.vim
+makeSymLink $PWD/plugins.vim $VIMDIR/plugins.vim
 makeSymLink $PWD/.aliases $HOME/.aliases
 
-checkOrMakeDir $HOME/.vim/colors
+checkOrMakeDir $VIMDIR/colors
 
 # Install Vundle if it is not present
-if [ ! -d "$HOME/.vim/bundle/Vundle.vim"  ]; then
+if [ ! -d "$VIMDIR/bundle/Vundle.vim"  ]; then
     printf "${GREEN}Vundle is not installed. Installation...${NC}\n"
-    git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+    git clone https://github.com/VundleVim/Vundle.vim.git $VIMDIR/bundle/Vundle.vim
 fi
 
-#Install monokai theme
-if [ ! -f "$HOME/.vim/colors/monokai.vim" ]; then
-    printf "${GREEN}The monokai color scheme is not installed. Installation...${NC}\n"
-    wget https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim -O $HOME/.vim/colors/monokai.vim
-fi
-
+#Install color themes
+checkOrDownload "https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim" "$VIMDIR/colors/monokai.vim"
+checkOrDownload "https://raw.githubusercontent.com/skielbasa/vim-material-monokai/master/colors/material-monokai.vim" "$VIMDIR/colors/material-monokai.vim"
+checkOrDownload "https://raw.githubusercontent.com/gosukiwi/vim-atom-dark/master/colors/atom-dark-256.vim" "$VIMDIR/colors/atom-dark-256.vim"
