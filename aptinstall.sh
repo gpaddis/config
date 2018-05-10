57315203f9a41268f6292e4c1ba374761a731f27
Gianpiero Addis <gianpiero.addis@gmail.com>
2018-05-10 14:00:32 +0200

    Install VS Code

--- aptinstall.sh
+++ aptinstall.sh
@@ -4,10 +4,9 @@ sudo apt update && sudo apt upgrade -y
 
 sudo apt remove vim-tiny -y
 
-sudo apt install vim vim-gtk xclip dia -y
+sudo apt install vim vim-gtk xclip dia curl -y
 
 # Install Zsh, make it the default shell, install oh-my-zsh
-
 if [ ! $SHELL = '/usr/bin/zsh' ]; then
     sudo apt install zsh -y
     chsh -s $(which zsh)
@@ -15,5 +14,18 @@ if [ ! $SHELL = '/usr/bin/zsh' ]; then
     # Install oh-my-zsh
     sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 else echo "Zsh is already installed."
+fi
+
+# Check if MS VS Code is installed
+if ! type "code" > /dev/null; then
+    # Add key and repository
+    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+    sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+    sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+    # Install code
+    sudo apt-get update
+    sudo apt-get install code -y
+else echo "Code is already installed."
 fi
 
