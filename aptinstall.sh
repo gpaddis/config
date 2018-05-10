49536c1812cf3cd51c4963ffe5f075cad4c7bda0
Gianpiero Addis <gianpiero.addis@gmail.com>
2018-05-10 13:43:01 +0200

    Install Zsh if it is not yet installed

--- aptinstall.sh
+++ aptinstall.sh
@@ -2,6 +2,18 @@
 
 sudo apt update && sudo apt upgrade -y
 
-sudo apt remove vim-tiny
+sudo apt remove vim-tiny -y
 
-sudo apt install vim xclip
+sudo apt install vim vim-gtk xclip dia -y
+
+# Install Zsh, make it the default shell, install oh-my-zsh
+
+if [ ! $SHELL = '/usr/bin/zsh' ]; then
+    sudo apt install zsh -y
+    chsh -s $(which zsh)
+
+    # Install oh-my-zsh
+    sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+else echo "Zsh is already installed."
+fi
+
