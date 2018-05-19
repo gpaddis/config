1a5dbd65e7e5e7b3b70f94ba9ed79859c83acc36
Gianpiero Addis <gianpiero.addis@gmail.com>
2018-05-19 19:25:17 +0200

    Edit headings

--- aptinstall.sh
+++ aptinstall.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# Install packages and do some configuration.
+## Install packages and do some configuration. ################################
 
 source bash_functions.sh
 
-# Get Ubuntu codename - in case something needs to be installed for a specific Ubuntu version
+## Get Ubuntu codename - in case something needs to be installed for a specific Ubuntu version
 UBUNTU_CODENAME="$(lsb_release -c | awk '{print $NF}')"
 
-# Add Repositories and Keys
+## Add Repositories and Keys ##################################################
 # VS Code 
 if ! type "code" > /dev/null; then
     curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
@@ -14,14 +14,14 @@ if ! type "code" > /dev/null; then
     sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 fi
 
-# Install the software required.
+## Install the software required. #############################################
 sudo apt update && sudo apt upgrade -y
 
 sudo apt remove vim-tiny -y
 
 sudo apt install vim vim-gtk colordiff xclip tree dia curl zim code zsh keepassx terminator fonts-powerline build-essential cmake python-dev python3-dev -y
 
-# Make zsh the default shell, install oh-my-zsh
+## Make zsh the default shell, install oh-my-zsh ##############################
 if [ ! $SHELL = '/usr/bin/zsh' ]; then
     $ZSHRC = $HOME/.zshrc
 
@@ -39,7 +39,7 @@ if [ ! $SHELL = '/usr/bin/zsh' ]; then
     echo "source ~/.aliases" >> $ZSHRC
 fi
 
-# Install PHPUnit 5, 6, 7
+## Install PHPUnit 5, 6, 7 ####################################################
 for i in $(seq 5 7);
 do
     if ! type "phpunit$i" > /dev/null; then
