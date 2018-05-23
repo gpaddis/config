f12fb7b55b59ead81290f84dacd304455041c805
Gianpiero Addis <gianpiero.addis@gmail.com>
2018-05-23 19:34:38 +0200

    Add fdupes

--- aptinstall.sh
+++ aptinstall.sh
@@ -19,7 +19,7 @@ sudo apt update && sudo apt upgrade -y
 
 sudo apt remove vim-tiny -y
 
-sudo apt install vim vim-gtk colordiff xclip tree dia curl zim code zsh keepassx terminator fonts-powerline build-essential cmake python-dev python3-dev oathtool gnupg2 -y
+sudo apt install vim vim-gtk colordiff xclip tree dia curl zim code zsh keepassx terminator fonts-powerline build-essential cmake python-dev python3-dev oathtool gnupg2 fdupes -y
 
 ## Make zsh the default shell, install oh-my-zsh ##############################
 if [ ! $SHELL = '/usr/bin/zsh' ]; then
