72a5dd76f1fb34ac34be71507544f208fc9dd59d
Gianpiero Addis <g.addis@lemundo.de>
2018-12-28 11:17:29 +0100

    Cleanup aptinstall.sh

--- aptinstall.sh
+++ aptinstall.sh
@@ -4,13 +4,13 @@
 
 . bash_functions.sh
 
-## Install packages and do some configuration. ################################
-## Get Ubuntu codename - in case something needs to be installed for a specific Ubuntu version
+# Get Ubuntu codename - in case something needs to be installed for a specific Ubuntu version
 UBUNTU_CODENAME="$(lsb_release -c | awk '{print $NF}')"
 
+# Initialization
 ZSHRC=$HOME/.zshrc
+checkOrMakeDir "$HOME/bin"
 
-## Add Repositories and Keys ##################################################
 # VS Code 
 if commandNotFound code; then
     curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
@@ -18,27 +18,26 @@ if commandNotFound code; then
     sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 fi
 
-## Install the software required. #############################################
+# Install the software required.
 sudo apt update && sudo apt upgrade -y
 
 sudo apt remove vim-tiny -y
 
 sudo apt install vim vim-gtk colordiff xclip tree dia curl zim code zsh keepassx terminator fonts-powerline build-essential cmake python-dev python3-dev oathtool gnupg2 fdupes gitk sqlitebrowser htop pinta geoip-bin ncdu tmux -y
 
-## Install fzf (https://github.com/junegunn/fzf) ##############################
+# Install fzf (https://github.com/junegunn/fzf)
 if [ ! -d $HOME/.fzf ]; then
-    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-    ~/.fzf/install
+    git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
+    $HOME/.fzf/install
 fi
 
-## Install cht.sh  ############################################################
+# Install cht.sh
 if fileNotFound "$HOME/bin/cht.sh"; then
-    # TODO: check if bin exists or create it
     curl https://cht.sh/:cht.sh > $HOME/bin/cht.sh
     chmod +x $HOME/bin/cht.sh
 fi
 
-## Make zsh the default shell, install oh-my-zsh ##############################
+# Make zsh the default shell, install oh-my-zsh
 if [ ! $SHELL = '/usr/bin/zsh' ]; then
     chsh -s $(which zsh)
     sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
@@ -54,7 +53,7 @@ if [ ! $SHELL = '/usr/bin/zsh' ]; then
     appendStringToFile "source ~/.aliases" $ZSHRC
 fi
 
-## Install PHPUnit 5, 6, 7 ####################################################
+# Install PHPUnit 5, 6, 7
 for i in $(seq 5 7);
 do
     if commandNotFound "phpunit$i"; then
@@ -64,7 +63,7 @@ do
     fi
 done 
 
-## Install Codeception ########################################################
+# Install Codeception
 if commandNotFound codecept; then
     sudo curl -LsS https://codeception.com/codecept.phar -o /usr/local/bin/codecept
     sudo chmod a+x /usr/local/bin/codecept
