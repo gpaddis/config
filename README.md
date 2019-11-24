bffd4b85538ccc582dac6f6103c401c1c225eb54
Gianpiero Addis <gianpiero.addis@gmail.com>
2019-11-24 10:24:26 +0100

    Update mac install list

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ Clone the repo in a folder in the home directory, then run the shell scripts.
 
 ### TBD
 - [ ] Add SSH passphrase to keychain: https://superuser.com/a/442691
-- [ ] Install [python 3.6](http://ubuntuhandbook.org/index.php/2017/07/install-python-3-6-1-in-ubuntu-16-04-lts/) (on Ubuntu <= 16.04)
 
 ### TODOs
 * Autoinstall PHP codesniffer
--- install-mac.sh
+++ install-mac.sh
@@ -2,9 +2,19 @@
 
 . bash_functions.sh
 
+# Install missing software with homebrew.
+installIfMissing() {
+    commandNotFound "$1" && brew install "$1"
+}
+
 commandNotFound brew && /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-commandNotFound zim && brew install zim
+installIfMissing fd
+installIfMissing zim
+installIfMissing tmux
+installIfMissing goaccess
+
+commandNotFound http && brew install httpie
 commandNotFound fzf && brew install fzf && $(brew --prefix)/opt/fzf/install
 
 printGreen "Installation complete."
