4df18d5d13f71840d064dbf127c3c9e23f7110d1
Gianpiero Addis <gianpiero.addis@gmail.com>
2019-12-13 19:30:39 +0100

    Install curl first

--- install-linux.sh
+++ install-linux.sh
@@ -14,23 +14,23 @@ checkOrMakeDir "$HOME/bin"
 # Import the custom aliases
 appendStringToFile "source ~/.aliases" $ZSHRC
 
-# VS Code
-if commandNotFound code; then
-    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-    sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-    sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-fi
-
 # Install the software required.
 sudo apt update && sudo apt upgrade -y
 
 sudo apt remove -y vim-tiny
 
 # Basic packages
-sudo apt install -y vim vim-gtk colordiff xclip tree curl zim code zsh keepassx fonts-powerline build-essential cmake python-dev python3-dev oathtool gnupg2 fdupes gitk sqlitebrowser htop geoip-bin ncdu tmux golang-go
+sudo apt install -y curl vim vim-gtk colordiff xclip tree curl zim code zsh keepassx fonts-powerline build-essential cmake python-dev python3-dev oathtool gnupg2 fdupes gitk sqlitebrowser htop geoip-bin ncdu tmux golang-go
 
 # Optional packages
 sudo apt install -y fd-find
+
+# VS Code
+if commandNotFound code; then
+    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+    sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+    sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+fi
 
 # Install fzf (https://github.com/junegunn/fzf)
 if [ ! -d $HOME/.fzf ]; then
