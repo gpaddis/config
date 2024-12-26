f726b1c587802b99fddab1e7c3d7aa633aca5b6d
Gianpiero Addis <gianpiero.addis@protonmail.com>
2024-12-26 22:45:02 +0100

    Update install-linux.sh

--- install-linux.sh
+++ install-linux.sh
@@ -20,7 +20,7 @@ sudo apt update && sudo apt upgrade -y
 sudo apt remove -y vim-tiny
 
 # Basic packages
-sudo apt install -y curl vim vim-gtk colordiff xclip tree curl zim code zsh keepassx fonts-powerline build-essential cmake python-dev python3-dev oathtool gnupg2 fdupes gitk sqlitebrowser htop geoip-bin ncdu tmux golang-go jq docker docker-compose
+sudo apt install -y curl vim vim-gtk colordiff xclip tree curl zim code zsh keepassxc fonts-powerline build-essential cmake python-dev python3-dev oathtool gnupg2 fdupes gitk sqlitebrowser htop geoip-bin ncdu tmux golang-go jq fzf
 
 # Optional packages
 sudo apt install -y fd-find
@@ -54,39 +54,8 @@ if [ ! $SHELL = '/usr/bin/zsh' ]; then
     appendStringToFile "DEFAULT_USER=\"$USER\"" $ZSHRC
 fi
 
-# Install PHPUnit 5, 6, 7
-for i in $(seq 5 7);
-do
-    if commandNotFound "phpunit$i"; then
-        sudo wget -O /usr/local/bin/phpunit$i https://phar.phpunit.de/phpunit-$i.phar
-        sudo chmod +x /usr/local/bin/phpunit$i
-        printGreen "Installed phpunit$i.\n"
-    fi
-done
-
-# Install Codeception
-if commandNotFound codecept; then
-    sudo curl -LsS https://codeception.com/codecept.phar -o /usr/local/bin/codecept
-    sudo chmod a+x /usr/local/bin/codecept
-    printGreen "Installed Codeception.\n"
-fi
-
 # Install Tmux Plugin Manager
 if fileNotFound "$HOME/.tmux/plugins/tpm/tpm"; then
     git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
     printGreen "Installed Tmux Plugin Manager.\n"
-fi
-
-# Install Tmuxinator
-if commandNotFound tmuxinator; then
-    sudo gem install tmuxinator
-    printGreen "Installed tmuxinator.\n"
-fi
-
-tmuxinatorCompletion="$HOME/bin/tmuxinator.zsh"
-if fileNotFound "$tmuxinatorCompletion"; then
-    wget -O $tmuxinatorCompletion https://raw.githubusercontent.com/tmuxinator/tmuxinator/master/completion/tmuxinator.zsh
-    chmod +x $tmuxinatorCompletion
-    appendStringToFile "source $tmuxinatorCompletion" $ZSHRC
-    printGreen "Tmuxinator completion added to path."
 fi
