fb29e47b689b27dfc14c781cf43263d0a44317d2
gp4d <gianpiero.addis@sapiens.com>
2025-02-05 09:41:46 +0100

    Generate a new ssh key and add it to ssh-agent

--- install-mac.sh
+++ install-mac.sh
@@ -25,9 +25,18 @@ commandNotFound http && brew install httpie
 commandNotFound fzf && brew install fzf && $(brew --prefix)/opt/fzf/install
 [ -z $(brew list | grep coreutils) ] && brew install coreutils
 
+fileNotFound $HOME/.ssh/id_ed25519 && {
+    echo -n "Enter your email: "
+    read EMAIL_ADDRESS
+    ssh-keygen -t ed25519 -C $EMAIL_ADDRESS
+}
+
 # Save the SSH passphrase in the Keychain.
+# https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
 grep -q 'UseKeychain yes' $HOME/.ssh/config || {
+    eval "$(ssh-agent -s)"
     echo -e "Host *\n    UseKeychain yes\n    AddKeysToAgent yes\n    IdentityFile ~/.ssh/id_ed25519" >> $HOME/.ssh/config
+    ssh-add --apple-use-keychain $HOME/.ssh/id_ed25519
 }
 
 # Install plugins
