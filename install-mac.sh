d1f7668dd2b170c72635e019588673f66f1c6f8c
gp4d <gianpiero.addis@sapiens.com>
2025-02-14 16:05:17 +0100

    Update install-mac

--- install-mac.sh
+++ install-mac.sh
@@ -3,33 +3,24 @@
 . bash_functions.sh
 
 # Install missing software with homebrew.
-installIfMissing() {
-  commandNotFound "$1" && brew install "$1"
-}
-
 commandNotFound brew && /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 commandNotFound zsh && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-installIfMissing fd
-installIfMissing jq
-installIfMissing zim
-installIfMissing nvim
-installIfMissing tmux
-installIfMissing ncdu
-installIfMissing openvpn
-installIfMissing goaccess
-installIfMissing redis
-installIfMissing git-absorb
-
-# Fonts
-installIfMissing font-fira-mono
-installIfMissing font-fira-code
-installIfMissing font-fira-code-nerd-font
-installIfMissing font-jetbrains-mono-nerd-font
-
-commandNotFound http && brew install httpie
 commandNotFound fzf && brew install fzf && $(brew --prefix)/opt/fzf/install
-[ -z $(brew list | grep coreutils) ] && brew install coreutils
+
+brew install coreutils \
+  fd \
+  goaccess \
+  httpie \
+  jq \
+  ncdu \
+  nvim \
+  openvpn \
+  redis \
+  tmux \
+  font-fira-mono \
+  font-fira-code \
+  font-fira-code-nerd-font \
+  font-jetbrains-mono-nerd-font
 
 fileNotFound $HOME/.ssh/id_ed25519 && {
   echo -n "Enter your email: "
@@ -46,19 +37,19 @@ grep -q 'UseKeychain yes' $HOME/.ssh/config || {
 }
 
 # Install plugins
-dirNotFound ~/.vim/bundle/Vundle.vim && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#dirNotFound ~/.vim/bundle/Vundle.vim && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 dirNotFound ~/.tmux/plugins/tpm && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 fileNotFound $HOME/.aliases && ln -s .aliases $HOME/.aliases
 grep -q '.aliases' $HOME/.zshrc || echo "source .aliases" >>$HOME/.zshrc
 
 # Configure neovim
-fileNotFound $HOME/.vimrc && ln -s .vimrc $HOME/.vimrc
-mkdir -p $HOME/.config/nvim/
-fileNotFound $HOME/.config/nvim/init.vim && cat <<EOT >$HOME/.config/nvim/init.vim
-set runtimepath^=~/.vim runtimepath+=~/.vim/after
-let &packpath=&runtimepath
-source ~/.vimrc
-EOT
+# fileNotFound $HOME/.vimrc && ln -s .vimrc $HOME/.vimrc
+# mkdir -p $HOME/.config/nvim/
+# fileNotFound $HOME/.config/nvim/init.vim && cat <<EOT >$HOME/.config/nvim/init.vim
+# set runtimepath^=~/.vim runtimepath+=~/.vim/after
+# let &packpath=&runtimepath
+# source ~/.vimrc
+# EOT
 
 printGreen "Installation complete."
